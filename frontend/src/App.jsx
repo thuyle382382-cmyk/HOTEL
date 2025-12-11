e161591d8930354d9cc36f39888f8d75dc4b8f3b
DSeaNguyen <nguyendinhhoangquan1825@gmail.com>
2025-12-11 21:12:31 +0700

    Tạo trang Guests

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { AppLayout } from "./components/AppLayout";
 import Dashboard from "./pages/Dashboard";
 import Rooms from "./pages/Rooms";
 import Bookings from "./pages/Bookings";
+import Guests from "./pages/Guests";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/rooms" element={<Rooms />} />
         <Route path="/bookings" element={<Bookings />} />
+        <Route path="/guests" element={<Guests />} />
       </Routes>
     </AppLayout>
   );
