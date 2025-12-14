093ea25021e70cd7c057b04d9cfcf71ad54b8123
Trần Quang Mạnh <manhtq0508@gmail.com>
2025-12-15 00:38:36 +0700

    chore: Delete outdate models

--- backend/models/Service.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ServiceSchema = new mongoose.Schema({
-  MaDV: { type: String, required: true, unique: true },
-  TenDV: { type: String, required: true },
-  DonGia: { type: Number, default: 0 },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Service', ServiceSchema);
--- backend/models/item.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const itemSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Item', itemSchema);
