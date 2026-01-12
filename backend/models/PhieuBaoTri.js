7b39bd9dd5b52b26860a94476898962385efb3d4
nmkhoi11042005 <23520777@gm.uit.edu.vn>
2026-01-13 00:48:21 +0700

    Update PhieuBaoTri.js

--- backend/models/PhieuBaoTri.js
+++ backend/models/PhieuBaoTri.js
@@ -1,24 +1,31 @@
 const mongoose = require('mongoose');
 
+
 const PhieuBaoTriSchema = new mongoose.Schema({
     MaPBT: { type: String, required: true, unique: true },
-    Phong: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Phong', 
-        required: true 
+    Phong: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Phong',
+        required: true
     },
-    NVKyThuat: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'NhanVien', 
-        required: true 
+    KhachHang: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'KhachHang',
+        required: true
+    },
+    NVKyThuat: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'NhanVien'
     },
     NoiDung: { type: String, required: true },
     NgayThucHien: { type: Date, default: Date.now },
     TrangThai: {
         type: String,
-        enum: ['Pending', 'Completed', 'Cancelled'],
+        enum: ['Pending', 'InProgress', 'Completed', 'Cancelled'],
         default: 'Pending'
     }
 }, { timestamps: true });
 
+
 module.exports = mongoose.model('PhieuBaoTri', PhieuBaoTriSchema);
+
